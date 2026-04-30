1b26a60b13d0c071d855936f42b69a663c4514f7
sachinkavindaa <sachikavindaa@gmail.com>
2026-04-29 21:39:28 -0500

    add scripts

---
+++ scripts/analysis_mapped_20sep2025.py
@@ -0,0 +1,34 @@
+f=open("/work/yinlab/mislam17/course_project_1/part_4/20250923_105539/analysis/GROUPS_140_Info.txt").readlines()[1:]
+g=open("/work/yinlab/mislam17/course_project_1/part_4/20250923_105539/NodeTable_cs.txt").readlines()[1:]
+char_ids=[x.rstrip().split("\t")[0] for x in open("/work/yinlab/mislam17/course_project_1/part_4/Characterized_ids_22_Sep_2025.txt").readlines()[1:]]
+subfams_old=[int(x.split()[1]) for x in open("/work/yinlab/mislam17/course_project_1/part_4/20250923_105539/analysis/GROUPS_140_Info.txt").readlines()[1:]]
+subfams=set([int(x.split()[1]) for x in open("/work/yinlab/mislam17/course_project_1/part_4/20250923_105539/analysis/GROUPS_140_Info.txt").readlines()[1:]])
+print (subfams)
+print ("Subfamily SubFam_seqs Char_seqs EC_nums")
+for x in sorted(subfams): # edit with index numbers if you want to run and check. but not recommended. [8:16] was there. 
+	#print (f"Subfamily {x}:",subfams_old.count(x))
+	sub_ids=[]
+	if subfams_old.count(x)>=20:
+		for i in f:
+			i=i.rstrip().split()
+			if int(i[1])==x:
+				for j in g:
+					j=j.rstrip().split()
+					if int(i[0])==int(j[0]):
+						#print (i,j)
+						if j[1].split("|")[0] not in sub_ids:
+							sub_ids.append(j[1].split("|")[0])
+	#print (sub_ids)
+	ids_common=set(sub_ids).intersection(set(char_ids))
+	#print (ids_common)
+	if len(ids_common)>=1:
+		ec_nums=[]
+		for id in ids_common:
+			for uu in open("/work/yinlab/mislam17/course_project_1/part_4/Characterized_ids_22_Sep_2025.txt").readlines()[1:]:
+				uu=uu.split("\t")
+				if id==uu[0]:
+					#print (uu[1].rstrip())
+					ec_nums.append(uu[1].rstrip().replace(" ", "").replace(",","|"))
+		print (f"Subfamily-{x}",subfams_old.count(x),len(set(ids_common)),"|".join(list(set(ec_nums))))
+	else:
+		pass
